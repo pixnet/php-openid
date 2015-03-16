d6d83a142c06f0695dce20f2b19295be8f4a2f10
afutseng <afu@pixnet.tw>
2015-03-16 17:53:04 +0800

    style: re-indent

--- Auth/Yadis/ParanoidHTTPFetcher.php
+++ Auth/Yadis/ParanoidHTTPFetcher.php
@@ -150,7 +150,7 @@ class Auth_Yadis_ParanoidHTTPFetcher extends Auth_Yadis_HTTPFetcher {
                     curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
                 }
             }
-                curl_setopt($c, CURLOPT_PROXY, $this->getAuthOpenIdHttpProxy());
+            curl_setopt($c, CURLOPT_PROXY, $this->getAuthOpenIdHttpProxy());
             curl_exec($c);
 
             $code = curl_getinfo($c, CURLINFO_HTTP_CODE);
@@ -210,7 +210,7 @@ class Auth_Yadis_ParanoidHTTPFetcher extends Auth_Yadis_HTTPFetcher {
             curl_setopt($c, CURLOPT_NOSIGNAL, true);
         }
 
-                curl_setopt($c, CURLOPT_PROXY, $this->getAuthOpenIdHttpProxy());
+        curl_setopt($c, CURLOPT_PROXY, $this->getAuthOpenIdHttpProxy());
 
         curl_setopt($c, CURLOPT_POST, true);
         curl_setopt($c, CURLOPT_POSTFIELDS, $body);
